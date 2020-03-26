5d4a79d6374ee69243467c0d79e199b61bab6a00
F-G Fernandez <fgfm03@hotmail.fr>
2020-03-26 19:55:42 +0100

    feat: Setup project (#1)
    * feat: Added all source files
    
    * chore: Updated gitignore
    
    * style: Fixed clang-format compliance
    
    * style: Added clang-format config
    
    * chore: Added CMakeLists

---
+++ src/tools.cpp
@@ -0,0 +1,67 @@
+#include "tools.h"
+#include <iostream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+using std::sqrt;
+using std::vector;
+
+Tools::Tools() = default;
+
+Tools::~Tools() = default;
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.empty() || estimations.size() != ground_truth.size()) {
+    cout << "Invalid argument dimensions" << endl;
+    return rmse;
+  }
+
+  // Compute the SSE
+  VectorXd tmp_(4);
+  for (uint i = 0; i < estimations.size(); ++i) {
+    tmp_ = (estimations[i] - ground_truth[i]).array().pow(2);
+    rmse += tmp_;
+  }
+
+  // Divide by the number of samples to get the MSE
+  rmse /= estimations.size();
+  // Take the squared root to get the RMSE
+  rmse = rmse.array().sqrt();
+
+  return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
+  // Initialize the Jacobian
+  MatrixXd J_(3, 4);
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // check division by zero
+  float denom = px * px + py * py;
+  if (denom == 0) {
+    cout << "Invalid measurement values" << endl;
+  } else {
+    float sq_denom = sqrt(denom);
+    float denom3_2 = denom * sq_denom;
+
+    // compute the Jacobian matrix
+    J_ << px / sq_denom, py / sq_denom, 0, 0, -py / denom, px / denom, 0, 0,
+        py * (vx * py - vy * px) / denom3_2,
+        px * (vy * px - vx * py) / denom3_2, px / sq_denom, py / sq_denom;
+  }
+
+  return J_;
+}
