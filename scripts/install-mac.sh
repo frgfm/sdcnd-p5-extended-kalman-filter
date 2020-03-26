1eff7a83013a327fdc9d50068ea0c64af69299fe
F-G Fernandez <fgfm03@hotmail.fr>
2020-03-26 21:19:35 +0100

    chore: Fixing uWebSockets install

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -5,6 +5,7 @@ mkdir tmp
 wget -P tmp/ https://github.com/uNetworking/uWebSockets/archive/v0.14.0.tar.gz
 tar -xvzf tmp/v0.14.0.tar.gz -C tmp/
 cd tmp/uWebSockets-0.14.0
+make
 sudo make install
 cd ../..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
